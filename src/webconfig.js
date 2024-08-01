0ebc3bc64772a42789553070322e4637c905b278
Donrskbb <65772844+Donrskbb@users.noreply.github.com>
2024-08-01 15:52:43 +0200

    Update webconfig.js

--- src/webconfig.js
+++ src/webconfig.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     // This is a test webhook and server, so replace this with you own data if you want to use
-    discordWebhookUrl: "https://canary.discord.com/api/webhooks/1263994431648829560/zH2ZzQzs5u-Pj4DnTgVYwskuHLt9GZXiOc7fxXHOJ0N3_JO2H-luhhkwIzMnnP5q-U1-",
+    discordWebhookUrl: "https://canary.discord.com/api/webhooks/1268566827710021683/VEFffZ-kgmrz2mSqNXisyE38Q4YNydlDJZTYQv-QXuZ3Fx1aquWQbZ_K7oqfq7WXOZHo",
     WebHookUrl: "http://node01.jammabot.nl",
     Port: 9109,
 };
